22ce526f2c56e744d0fa4edd43f70634f8a986fd
Ilham Dirgantara <ilhamdirgantara682@gmail.com>
2023-02-13 22:25:38 +0700

    learn update-dependencies-from-library

--- NodeJS/belajar-nodejs-npm/package.js
+++ NodeJS/belajar-nodejs-npm/package.js
@@ -1,6 +1,10 @@
 import {hello, sum} from "belajar-nodejs-npm-library-dirgan";
+import {max, min} from "belajar-nodejs-npm-library-dirgan/filter";
 
 console.info(hello("ilham"));
 
 const numbers = [1,2,3,4,5];
 console.info(sum(numbers));
+
+console.info(min(1,2));
+console.info(max(1,2));
