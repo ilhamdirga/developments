03caed17c88e73e9ec24564dc95773e07206a8c0
Ilham Dirgantara <ilhamdirgantara682@gmail.com>
2023-03-09 23:05:28 +0700

    belajar-nodejs-logging

---
+++ NodeJS/belajar-nodejs-logging/test/transport.test.js
@@ -0,0 +1,33 @@
+import winston from "winston";
+import TransportStream from "winston-transport";
+
+test("learn logging with winston-transport", () => {
+
+    class MyTransport extends TransportStream {
+
+        constructor(option) {
+            super(option);
+        }
+
+        log(info, next) {
+            console.log(`${new Date()} : ${info.level.toUpperCase()} : ${info.message}`);
+            next()
+        }
+
+    }
+
+    const logger = winston.createLogger({
+        level:"silly",
+        transports: [
+            new MyTransport({})
+        ]
+    });
+
+    logger.error("Hello Error");
+    logger.warn("Hello Warn");
+    logger.info("Hello Info");
+    logger.http("Hello HTTP");
+    logger.verbose("Hello Verbose");
+    logger.debug("Hello Debug");
+    logger.silly("Hello Silly");
+})
